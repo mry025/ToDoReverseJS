7d6550fc865d56f1684f17dd0ee7a648a6a72893
nothing <2727219897@qq.com>
2024-12-04 19:27:13 +0800

    新增fileControl

---
+++ src/fileControl.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const path = require('path');
+
+function readFile(filePath, encode='utf8')
+{
+    const data = fs.readFileSync(filePath, encode);
+    return data;
+}
+
+function writeFile(filePath, data)
+{
+    let dirPath = path.dirname(filePath);
+    !fs.existsSync(dirPath) && fs.mkdirSync(dirPath);
+
+    fs.writeFileSync(filePath, data);
+}
+
+class WriteDir
+{
+    constructor(dir, defaulfName="", type=".txt")
+    {
+        if (typeof dir != "string") throw new Error("传入类型有误！");
+
+        this.dir = dir;
+        this.defaulfName = defaulfName;
+        this.type = type;
+        this.writeCount = 0;
+    }
+
+    writeFile(filePath, data)
+    {
+        let dirPath = path.dirname(filePath);
+        !fs.existsSync(dirPath) && fs.mkdirSync(dirPath);
+    
+        fs.writeFileSync(filePath, data);
+    }
+
+    write(data, name)
+    {
+        if (name == undefined) 
+        {
+            this.writeCount++;
+            name = this.defaulfName + this.writeCount;
+        }
+        else
+        {
+            this.defaulfName = name;
+            this.writeCount = 0;
+        }
+
+        let filePath = path.join(this.dir, name) + this.type;
+        this.writeFile(filePath, data);
+    }
+}
+
+module.exports = {
+    WriteDir,
+    writeFile,
+    readFile
+}
